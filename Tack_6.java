63c71173ec0670f83f945420b741687189a03665
Liuchengxun <1367469967@qq.com>
2017-07-13 17:19:13 +0800

    Add files via upload

---
+++ Tack_6.java
@@ -0,0 +1,43 @@
+/*import java.util.Scanner;
+
+public class Tack_6 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+       Scanner input = new Scanner(System.in);
+       double score=0;
+       double sum =0;
+       double averageScore=0;
+       for(int i=1;i<=5;i++){
+    	   System.out.print("请输入第"+i+"天学习的时间：");
+    	   score=input.nextDouble();
+    	   sum=score+sum;
+    	   continue;  
+	}
+      averageScore=sum/5;
+       System.out.print("平均时间为"+averageScore);
+	}
+}
+*/
+
+
+/*import java.util.Scanner;
+
+public class Tack_6 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+       Scanner input = new Scanner(System.in);
+       for(int i=0;i<=100;i++){
+    	   if(i%3==0){
+    		   System.out.println("Flip");
+    	   }else if(i%5==0){
+    		   System.out.println("Flop");
+    	   }else if((i%3==0)&&(i%5==0)){
+    		   System.out.println("FlioFlop");
+    	   }else{
+    		   System.out.println(i);
+    	   }
+       }
+	}
+}*/
